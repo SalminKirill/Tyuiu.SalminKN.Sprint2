089ed723f2aaa5b5d6793e6b720def5bc45ca9ed
Tyuiu.SalminKN@outlook.com <Tyuiu.SalminKN@outlook.com>
2023-10-07 18:57:26 +0500

    Add project files.

---
+++ Tyuiu.SalminKN.Sprint2.Task0.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SalminKN.Sprint2.Task0.V0.Lib;
+namespace Tyuiu.SalminKN.Sprint2.Task0.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 15;
+            int y = 16;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperation(x, y);
+            bool[] wait = new bool[6] { true, true, true, true, true, true };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SalminKN.Sprint2.Task0.V0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SalminKN.Sprint2.Task0.V0.Lib;
+namespace Tyuiu.SalminKN.Sprint2.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 15;
+            int y = 16;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
